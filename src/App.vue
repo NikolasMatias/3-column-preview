2e451aee5f725966a16d6df3e934513b0f571bd6
Nikolas Matias <nikolas.matias500@gmail.com>
2021-07-31 19:29:23 -0300

    Delete of some non-used files, update the texts inside of each vehicle and partial update of README.md

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
     <section class="vehicle">
       <img class="section-image" src="/images/icon-sedans.svg"/>
       <h2 class="section-title">SEDANS</h2>
-      <p class="section-content">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+      <p class="section-content">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
       <button class="section-button">Learn More</button>
     </section>
     <section class="vehicle">
       <img class="section-image" src="/images/icon-suvs.svg"/>
       <h2 class="section-title">SUVS</h2>
-      <p class="section-content">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+      <p class="section-content">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
       <button class="section-button">Learn More</button>
     </section>
     <section class="vehicle">
       <img class="section-image" src="/images/icon-luxury.svg"/>
       <h2 class="section-title">LUXURY</h2>
-      <p class="section-content">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip</p>
+      <p class="section-content">Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
       <button class="section-button">Learn More</button>
     </section>
   </div>
