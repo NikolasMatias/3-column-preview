e98f4e06e1cacff5f95b019e02034f95142e03b3
Nikolas Matias <nikolas.matias500@gmail.com>
2021-07-31 20:14:27 -0300

    Adding alt for my images

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="vehicles">
     <section class="vehicle">
-      <img class="section-image" src="/images/icon-sedans.svg"/>
+      <img class="section-image" src="/images/icon-sedans.svg" alt="Icon showing a sedans car"/>
       <h2 class="section-title">SEDANS</h2>
       <p class="section-content">Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.</p>
       <button class="section-button">Learn More</button>
     </section>
     <section class="vehicle">
-      <img class="section-image" src="/images/icon-suvs.svg"/>
+      <img class="section-image" src="/images/icon-suvs.svg" alt="Icon showing a suvs car"/>
       <h2 class="section-title">SUVS</h2>
       <p class="section-content">Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.</p>
       <button class="section-button">Learn More</button>
     </section>
     <section class="vehicle">
-      <img class="section-image" src="/images/icon-luxury.svg"/>
+      <img class="section-image" src="/images/icon-luxury.svg" alt="Icon showing a luxury car"/>
       <h2 class="section-title">LUXURY</h2>
       <p class="section-content">Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.</p>
       <button class="section-button">Learn More</button>
